83f678ae9dd23bf3600265a8166aa28187675275
Daniela <danigami159@hotmail.com>
2023-01-31 20:03:22 +0100

    Juego adivinar sin completar correctamente =(

--- TerceraSemana/src/Juego/Adivina/Adivina.java
+++ TerceraSemana/src/Juego/Adivina/Adivina.java
@@ -1,82 +1,126 @@
-package Juego;
+package Juego.Adivina;
 
 import java.util.Random;
 import java.util.Scanner;
 
 /**
- * Realizar un juego para adivinar un número , para ello obtenerlo 
- * aleatoriamente un número , y luego ir pidiendo al usuario los
- * números indicando sí es mayor o menor según sea mayor o menos
- * con respecto a N. El proceso termina cuando el usuario acuerte.
+ * Realizar un juego para adivinar un número , para ello obtenerlo
+ * aleatoriamente un número , y luego ir pidiendo al usuario los números
+ * indicando sí es mayor o menor según sea mayor o menos con respecto a N. El
+ * proceso termina cuando el usuario acuerte.
  * 
  * @author d.garcia.millan
  *
  */
 public class Adivina {
-	static int topeSub = 100;
-	static int topeInf = 0;
-	static int miNum = 20;
-	
-	public static void adivinaAdivinanza1() {
-		int numero=(int)(Math.random()*100)+1;
-		
-		try (Scanner lectura = new Scanner (System.in)) {
-			System.out.println("Ingrese el número a adivinar ");
-			int numUser = lectura.nextInt();
-			
-			 do{
-				if(numUser > numero)
-					System.out.println(numUser + " es menor");
-				else
-					System.out.println(numUser + " es mayor");
-				System.out.println("Ingrese número: ");
-				numUser = lectura.nextInt();
-			}while(numUser!=numero);
-			 
-			if(numUser == numero)
-				System.out.println("Has acertado !!");
-		}
-	}
-	public static void adivinaAdivinanza2() {
-		int nRandom=(int)(Math.random()*100)+1;
-		String numCadena= "";
-		
-		try (Scanner lectura = new Scanner (System.in)) {
-			System.out.println("Ingrese el número a adivinar ");
-			int numUser = lectura.nextInt();
-			
-			while(numUser != nRandom || numUser != miNum) {
-				System.out.print("Némero:" + nRandom);
-				if(numUser != miNum) {
-					System.out.print("\t (M)ayor \\t (Me)nor \\t (f)in");
-					numCadena = lectura.next();
+	static boolean finPartida;
+	static int maquina = (int) (Math.random() * 1000) + 1;
+	static int usuario = (int) (Math.random() * 1000) + 1;
+	static int limInf = 0;
+	static int limSup = 1000;
+	static boolean ganamaquina;
+	static boolean ganaUsuario;
+	static int resUsuairo = 0;
 
-					if(numCadena.equalsIgnoreCase("M") && ( numUser < miNum || numUser <  nRandom)) {
-						System.out.println("Introduce un número: ");
-						numUser = lectura.nextInt();
-						if(numUser == nRandom || numUser == miNum) {
-							System.out.println("Acertaste");
-							break;
-						}
-					}
-					else if(numCadena.equalsIgnoreCase("Me") && ( numUser > miNum || numUser >  nRandom)) {
-						System.out.println("Introduce un número: ");
-						numUser = lectura.nextInt();
-						if(numUser == nRandom || numUser == miNum) {
-							System.out.println("Acertaste");
-							break;
-						}
-					}else if(numCadena.equalsIgnoreCase("f") && ( numUser < miNum || numUser <  nRandom)) {
-						System.out.println("Gracias por participar, chao !!");
-						break;
-					}
+	static Scanner lectura = new Scanner(System.in);
+
+	public static void adivinaAdivinanza1() {
+		int numero = (int) ((Math.random() * 100) + 1);
+
+		System.out.println("Ingrese el número a adivinar ");
+		int numUser = lectura.nextInt();
+
+		do {
+			if (numUser > numero)
+				System.out.println(numUser + " es menor");
+			else if (numUser < numero) {
+				System.out.println(numUser + " es mayor");
+			}
+			System.out.println("Ingrese número: ");
+			numUser = lectura.nextInt();
+		} while (numUser != numero);
+
+		if (numUser == numero)
+			System.out.println("Has acertado !!");
+	}
+
+	public static void adivinaAdivinanza2() {
+		while (finPartida == false) {
+			try {
+				if (ganamaquina == false) {
+					tiradaMaquina();
+				} else if (ganaUsuario == false) {
+					tiradaUsuario();
 				}
+			} catch (Exception e) {
+				e.getMessage();
+				lectura.close();
 			}
 		}
 	}
+
+	public static void tiradaMaquina() {
+		String respuesta = "";
+
+		while (resUsuairo != maquina) {
+			System.out.print("Número" + maquina + "\t (M)ayor \t (Me)nor \t (f)in");
+			respuesta = lectura.next().toUpperCase();
+
+			switch (respuesta) {
+			case "M":
+				maquina = limInf;
+				break;
+			case "Me":
+				maquina = limSup;
+				break;
+			case "f":
+				System.out.println("Te gane !!");
+				ganamaquina = true;
+				finPartida = true;
+			}
+		}
+	}
+
+	public static void tiradaUsuario() {
+		System.out.println("Ingrese el número a adivinar ");
+		resUsuairo = lectura.nextInt();
+
+		if (resUsuairo > usuario)
+			System.out.println(resUsuairo + " es menor");
+		else if (resUsuairo < usuario) {
+			System.out.println(resUsuairo + " es mayor");
+		} else if (resUsuairo == usuario) {
+			System.out.println("Acertaste !!");
+			ganaUsuario = true;
+			finPartida = true;
+		}
+	}
+
 	public static void main(String[] args) {
-		adivinaAdivinanza1();
+		// adivinaAdivinanza1();
 		adivinaAdivinanza2();
 	}
 }
-	
+/**
+ * int nRandom=(int)(Math.random()*100)+1; String numCadena= ""; try (Scanner
+ * lectura = new Scanner (System.in)) { System.out.println("Ingrese el número a
+ * adivinar "); int numUser = lectura.nextInt();
+ * 
+ * while(numUser != nRandom || numUser != miNum) {
+ * 
+ * System.out.print("Número:" + nRandom); if(numUser != miNum) {
+ * System.out.print("\t (M)ayor \\t (Me)nor \\t (f)in"); numCadena =
+ * lectura.next();
+ * 
+ * if(numCadena.equalsIgnoreCase("M") && ( numUser < miNum || numUser <
+ * nRandom)) { System.out.println("Introduce un número: "); numUser =
+ * lectura.nextInt(); if(numUser == nRandom || numUser == miNum) {
+ * System.out.println("Acertaste"); break; } } else
+ * if(numCadena.equalsIgnoreCase("Me") && ( numUser > miNum || numUser >
+ * nRandom)) { System.out.println("Introduce un número: "); numUser =
+ * lectura.nextInt(); if(numUser == nRandom || numUser == miNum) {
+ * System.out.println("Acertaste"); break; } }else
+ * if(numCadena.equalsIgnoreCase("f") && ( numUser < miNum || numUser <
+ * nRandom)) { System.out.println("Gracias por participar, chao !!"); break; } }
+ * } }
+ */
